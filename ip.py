e81182de751ea73d511186d36d1e69c503c6ebd1
vpn62399 <113651623+vpn62399@users.noreply.github.com>
2023-06-16 11:13:57 +0900

    Update ip.py
    HOSTS

--- ip.py
+++ ip.py
@@ -3,21 +3,32 @@
 import socket
 import pygame
 
-HOST = 'www.google.com'
+
 PORT = 80
 BUFFER_SIZE = 1024
+HOSTS = ['www.google.com', '192.168.1.20','192.168.1.247']
 
 pygame.mixer.init()
 alarm_sound = pygame.mixer.Sound('alarm.wav')
 
-while True:
-    try:
+def net(HOST):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         s.connect((HOST, PORT))
         s.sendall(b'GET / HTTP/1.1\r\n\r\n')
         data = s.recv(BUFFER_SIZE)
         s.close()
-        pygame.time.wait(5000)  # 
+        return
+
+while True:
+    try:
+        for v in HOSTS:
+            HOST = v
+            net(v)
+            pygame.time.wait(500) 
     except:
+        print(v)
         alarm_sound.play()
-        pygame.time.wait(5000)  # 
+        pygame.time.wait(500)  # 
+
+
